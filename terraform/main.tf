e24fdc2aad22e1978b8443b42c66143560af99c6
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2025-11-05 22:46:00 +0200

    Added Terraform files just for trial!

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0ecb62995f68bb549"  
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Test"
+  }
+}
+
+resource "aws_instance" "test_instance2" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Test-2"
+  }
+}
